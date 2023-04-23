6630c2d7ba31b6fb0f54597ff09f8fa758b71ce5
NonGP33M <phumrapee.sk@gmail.com>
2023-04-23 20:52:07 +0700

    Add react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Footer from './components/Footer';
 import Header from './components/Header'
-import Home from './components/Home';
+import Home from './pages/Home';
+import Signin from './pages/Signin';
 
 function App() {
   return (
-    <>
-      <Header/>
-      <Home/>
-      <Footer/>
-    </>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign_in" element={<Signin />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter> 
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,27 +1,50 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 function Header() {
+  let isLogin = false;
   return (
-    <div className='container fixed z-50 bg-zinc-900 shadow flex justify-between w-full h-20 min-w-full items-center font-kanit drop-shadow-xl'>
-        <div className='flex text-white mx-12 text-xl'>
+    <div className="container fixed z-50 bg-zinc-900 shadow flex justify-between w-full h-20 min-w-full items-center font-kanit drop-shadow-xl">
+      <div className="flex text-white mx-12 text-xl">
+        <Link className="mx-4 text-xl" to="/#">
           [Title]
-        </div>
+        </Link>
+      </div>
 
-        {/* Before Signed In */}
-        <div className='flex mx-12 text-white justify-between items-center'>
-            <a className='mx-4 text-xl' href='/#'>Sign in</a>
-            <a className='mx-4 text-xl rounded-lg border-2 py-1 px-2' href='/#'>Sign up</a>
-        </div>
-
-        {/* After Signed In */}
-        {/* <div className='flex mx-12 text-white justify-between'>
-            <a className='mx-4 text-xl' href='/#'>Home</a>
-            <a className='mx-4 text-xl' href='/#'>Order</a>
-            <a className='mx-4 text-xl' href='/#'>Scoreboard</a>
-            <a className='mx-4 text-xl' href='/#'>[Username]</a>
-        </div> */}
+      {/* Before Signed In */}
+      <div className="flex mx-12 text-white justify-between items-center">
+        {!isLogin && (
+          <div>
+            <Link className="mx-4 text-xl" to="/sign_in">
+              Sign in
+            </Link>
+            <Link
+              className="mx-4 text-xl rounded-lg border-2 py-1 px-2"
+              to="/sign_up"
+            >
+              Sign up
+            </Link>
+          </div>
+        )}
+        {isLogin && (
+          <div>
+            <Link className="mx-4 text-xl" to="/#">
+              Home
+            </Link>
+            <Link className="mx-4 text-xl" to="/order">
+              Order
+            </Link>
+            <Link className="mx-4 text-xl" to="/scoreboard">
+              Scoreboard
+            </Link>
+            <Link className="mx-4 text-xl" to="/#">
+              [Username]
+            </Link>
+          </div>
+        )}
+      </div>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
