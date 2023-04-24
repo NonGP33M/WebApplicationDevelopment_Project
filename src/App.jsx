ba3012df0664c0ae10b3ab4f1399e4d98a334b71
Woramet <68908184+nekoham@users.noreply.github.com>
2023-04-24 18:09:43 +0700

    Adding scoreboard

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Footer from './components/Footer';
 import Header from './components/Header'
 import Home from './pages/Home';
 import Signin from './pages/Signin';
+import Scoreboard from './pages/Scoreboard';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sign_in" element={<Signin />} />
+        <Route path="/scoreboard" element={<Scoreboard /> } />
       </Routes>
       <Footer />
     </BrowserRouter> 
